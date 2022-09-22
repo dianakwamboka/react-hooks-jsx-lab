c1a29b684040f713240507ffc229d8850a7d67bc
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-09-23 02:32:43 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,17 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+    </nav>
+
+return (
+  <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+  </nav>
+) 
 }
 
 export default NavBar;
